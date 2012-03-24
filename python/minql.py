0caa2e6f2ecda893a291c9086bca22a0552a5714
Ganesh Katrapati <neshmailsu@gmail.com>
2012-03-24 23:23:07 +0530

    License

--- python/minql.py
+++ python/minql.py
@@ -1,3 +1,22 @@
+#Copyright 2012
+#Ganesh Katrapati <ganesh.katrapati@gmail.com>
+#This file is part of MinQL.
+
+#MinQL is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#MinQL is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with MinQl. If not, see <http://www.gnu.org/licenses/>.
+
+
+
 #Basic Import Statements
 import re
 
